32da8761afedb0d7966bf4bd62159960a8ab72a8
Fan Zeng <fanzengau@gmail.com>
2019-09-03 12:22:05 +1000

    test express

--- express/hello.js
+++ express/hello.js
@@ -1,6 +1,26 @@
-exports.handler = function(event, context, callback) {
-	callback(null, {
-		statusCode: 200,
-		body: 'Hellooooo, World'
-	})
-}
+// exports.handler = function(event, context, callback) {
+// 	callback(null, {
+// 		statusCode: 200,
+// 		body: 'Hellooooo, World'
+// 	})
+// }
+
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+router.get('/', (req, res) => {
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	res.write('<h1>hello from express</h1>');
+	res.end();
+});
+
+app.use('/.netlify/functions/hello', router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
