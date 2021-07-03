b76e73e926bf532ee956a0d8e363d37c896edb2e
Fan Zeng <fanzengau@gmail.com>
2021-07-03 17:35:30 +1000

    linting

--- public/script/share_linkedin.js
+++ public/script/share_linkedin.js
@@ -1,24 +1,23 @@
-const url_signin='https://api.linkedin.com/v2/me';
+const url_signin = 'https://api.linkedin.com/v2/me';
 
-
-$(document).ready(function(){
-    $("#share_linkedin").click(function(){
-        console.log('clicked');
-      $.ajax({
-        url: url_signin,
-        type: 'get',
-        xhrFields: {
-            withCredentials: true
+$(document).ready(function () {
+  $("#share_linkedin").click(function () {
+    console.log('clicked');
+    $.ajax({
+      url: url_signin,
+      type: 'get',
+      xhrFields: {
+        withCredentials: true
+      },
+      data: {},
+      success:
+        (response) => {
+          console.log(`response is: ${response}`)
         },
-        data: {},
-        success:
-        (response)=>{
-            console.log(`response is: ${response}`)
-        },
-        error: (xhr, status)=> {
-            console.log(`xhr is ${xhr} and status is ${status}`);
-        }
-      });
+      error: (xhr, status) => {
+        console.log(`xhr is ${xhr} and status is ${status}`);
+      }
     });
+  });
 });
 
--- site-include/siteInclude.js
+++ site-include/siteInclude.js
@@ -1,11 +1,11 @@
 /*
 Usage:
 (0)
-siteInclude.getStaticLocalFile( 'absolute/path/from/site/root/without/initial/slash',
-								'avoid_rsync_disaster.html')
-	.then(
-		data => { console.log('data=', data) }
-	);
+siteInclude.getStaticLocalFile('absolute/path/from/site/root/without/initial/slash',
+                'avoid_rsync_disaster.html')
+  .then(
+    data => { console.log('data=', data) }
+  );
 (1)
 let siteHeader = await siteInclude.getInclude(hostName, pathInclude, 'site_header.html');
 */
@@ -19,61 +19,50 @@ const util = require('util');
 const readFile = util.promisify(fs.readFile);
 
 async function getStaticLocalFile(absPath, fileName) {
-	let dataString = "";
-	try {
-		const data = await readFile(path.join(absPath, fileName), 'utf-8');
-		dataString = data.toString('utf8');
-		console.log("In getStaticLocalFile: dataString=", dataString);
-		return dataString;
-	} catch (err) {
-		console.log("In getStaticLocalFile: ",  err);
-	}
+  let dataString = '';
+  try {
+    const data = await readFile(path.join(absPath, fileName), 'utf-8');
+    dataString = data.toString('utf8');
+    return dataString;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function getInclude(hostName, path_, name_) {
+  let portNumber;
+  let httpToUse;
+  if (hostName === 'localhost') {
+    portNumber = 3000;
+    httpToUse = http;
+  } else {
+    portNumber = 443;
+    httpToUse = https;
+  }
+  let options = {
+    host: hostName,
+    port: portNumber,
+    path: path.join(path_, name_) || ' '
+  };
 
-	let portNumber;
-	let httpToUse;
-
-	if (hostName === 'localhost') {
-		portNumber = 3000;
-		httpToUse = http;
-	} else {
-		portNumber = 443;
-		httpToUse = https;
-	}
-	let options = {
-	  host: hostName,
-	  port: portNumber,
-	  path: path.join(path_, name_) || ' '
-	};
-	console.log(
-		'start to get url: ' +
-		options.host +
-		':' + options.port + 
-		options.path
-		
-	);
-
-	let content = '';
-	await new Promise((resolve, reject) => {
-		let req = httpToUse.request(options, function(res) {
-		    res.setEncoding("utf8");
-		    res.on("data", function (chunk) {
-		        content += chunk;
-		    });
-		    res.on("end", function () {
-		    	// console.log('getInclude recieved the following content: ' + content);
-		    	resolve(content);
-		    });
-		    res.on('error', function(err) {
-				console.log(error);
-				reject(error);
-			});
-		});
-		req.end();
-	});
-	return content;
+  let content = '';
+  await new Promise((resolve, reject) => {
+    let req = httpToUse.request(options, function (res) {
+      res.setEncoding("utf8");
+      res.on("data", function (chunk) {
+        content += chunk;
+      });
+      res.on("end", function () {
+        resolve(content);
+      });
+      res.on('error', function (err) {
+        console.log(error);
+        reject(error);
+      });
+    });
+    req.end();
+  });
+  return content;
 }
 
 exports.getInclude = getInclude;
