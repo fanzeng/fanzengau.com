14a202aedb70c6203044cd988d773ad7290a2101
Fan Zeng <fanzengau@gmail.com>
2023-10-03 01:07:36 +1000

    update MathJax component

--- client/fanzengau-app/src/myblog/math-jax/MathJax.jsx
+++ client/fanzengau-app/src/myblog/math-jax/MathJax.jsx
@@ -9,11 +9,12 @@ export function MathJax() {
   useEffect(() => {
     const script = document.createElement('script');
     script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML";
-    script.async = true;
-    document.body.appendChild(script);
-    console.log('MathJax rendered.')
+    script.defer = true;
+    document.head.appendChild(script);
+    console.log('MathJax loaded.')
     return () => {
-      document.body.removeChild(script);
+      document.head.removeChild(script);
+      console.log('MathJax unloaded.')
     }
   }, []);
 }
