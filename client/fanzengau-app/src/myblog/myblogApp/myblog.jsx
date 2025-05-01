9c457bb4a3addfd66e2eaa37cc8dd31131971d46
Fan Zeng <fanzengau@gmail.com>
2025-05-01 11:32:40 +0000

    Improve styles

--- client/fanzengau-app/src/myblog/myblogApp/myblog.jsx
+++ client/fanzengau-app/src/myblog/myblogApp/myblog.jsx
@@ -1,17 +1,16 @@
 import { Link } from 'react-router-dom';
+import styles from './myblog.css';
 
 export function Myblog() {
-  // Dynamically import all JSX files from the myblog/content directory
   const importBlogs = (requireContext) => {
     const blogs = requireContext.keys().map((key) => {
       const pathParts = key.split('/');
       const fileName = pathParts[pathParts.length - 1];
-      const title = fileName.replace(/_/g, ' ').replace('.jsx', ''); // Format title
-      const url = `/myblog/content${key.replace('./', '').replace('.jsx', '')}`; // Generate URL
+      const title = fileName.replace(/_/g, ' ').replace('.jsx', '');
+      const url = `/myblog/content${key.replace('./', '').replace('.jsx', '')}`;
       return { title, url, pathParts };
     });
 
-    // Organize blogs into a nested structure
     const nestedBlogs = {};
     blogs.forEach(({ title, url, pathParts }) => {
       let currentLevel = nestedBlogs;
@@ -22,14 +21,12 @@ export function Myblog() {
         if (!currentLevel[part]) currentLevel[part] = {};
         currentLevel = currentLevel[part];
 
-        // Stop descending further if the directory matches the file name
         if (index === pathParts.length - 3 && part === fileNameWithoutExtension) {
           currentLevel.title = dirName.replace(/_/g, ' ');
           currentLevel.url = url;
         }
       });
 
-      // Add the file as a leaf node only if it's not part of a matching directory
       if (dirName !== fileNameWithoutExtension) {
         currentLevel[pathParts[pathParts.length - 1]] = { title, url };
       }
@@ -39,25 +36,24 @@ export function Myblog() {
   };
 
   const blogs = importBlogs(
-    require.context('../content', true, /\.jsx$/) // Only include JSX files
+    require.context('../content', true, /\.jsx$/)
   );
 
-  // Recursive function to render the nested structure
   const renderNestedBlogs = (nestedBlogs) => {
     return (
-      <ul>
+      <ul className="blogList">
         {Object.entries(nestedBlogs).map(([key, value]) => {
           if (value.url) {
-            // Leaf node (directory with matching .jsx file or blog file)
             return (
-              <li key={value.url}>
-                <Link to={value.url}>{value.title}</Link>
+              <li key={value.url} className="blogItem">
+                <Link to={value.url} className="blogLink">
+                  {value.title}
+                </Link>
               </li>
             );
           } else {
-            // Directory (nested blogs)
             return (
-              <li style={{ fontSize: 15 }} key={key}>
+              <li key={key} className="blogCategory">
                 <strong>{key.replace(/_/g, ' ')}</strong>
                 {renderNestedBlogs(value)}
               </li>
@@ -68,5 +64,5 @@ export function Myblog() {
     );
   };
 
-  return <div style={{ margin: '5px' }}>{renderNestedBlogs(blogs)}</div>;
+  return <div className={styles.blogContainer}>{renderNestedBlogs(blogs)}</div>;
 }
--- client/fanzengau-app/src/portfolio/certificates/Certificates.jsx
+++ client/fanzengau-app/src/portfolio/certificates/Certificates.jsx
@@ -1,80 +1,97 @@
+import './Certificates.css';
+
 export function Certificates({certificateImages}) {
   return <>
   <div className="section">
-  <a className="intra_page_link" id="certificates"></a>
-  <h2>
-    Certificates
-  </h2>
-  <div>
-    <div className="center_h">
-      <img src={certificateImages['ScrumAlliance_CSM_Certificate.png']} height="618" width="798"
-      alt="ScrumAlliance CSM Certificate"/>
-    </div>
-    <div className="center_h">
-      <a href="https://www.freecodecamp.org/certification/fanzeng/responsive-web-design">
-        <img src={certificateImages['freecodecamp_responsive_web_design.png']} height="476" width="840"
-          alt="Responsive Web Design"/>
-      </a><br/>
-      <a href="https://www.freecodecamp.org/certification/fanzeng/javascript-algorithms-and-data-structures">
-        <img src={certificateImages['freecodecamp_javascript_algorithms_and_data_structures.png']}
-          height="476" width="840" alt="Javascript Algorithms and Data Structures"/>
-      </a><br/>
-      <a href="https://www.freecodecamp.org/certification/fanzeng/front-end-development-libraries">
-        <img src={certificateImages['freecodecamp_front_end_development_libraries.png']}
-          height="476" width="840" alt="Front End Development Libraries"/>
-      </a><br/>      
-    </div>
-    <div className="center_h">
-      <div>
-        <table style={{textAlign: 'center'}}>
-          <tr>
-            <td><img src={certificateImages['hackerrank_java_gold_level.png']}
-                alt="HackerRank Java Gold level"/></td>
-          </tr>
-          <tr>
-            <td><a href="https://www.hackerrank.com/fanzeng" className="center_h">HackerRank Java Gold level</a>
-            </td>
-          </tr>
-        </table>
+    <a className="intra_page_link" id="certificates"></a>
+    <h2>
+      Certificates
+    </h2>
+    <div>
+      <div className="certificates-grid">
+        <div className="certificate-item">
+          <img src={certificateImages['ScrumAlliance_CSM_Certificate.png']} height="200" width="260"
+            alt="ScrumAlliance CSM Certificate" />
+          <a href="#" className="certificate-name">ScrumAlliance CSM Certificate</a>
+        </div>
+        <div className="certificate-item">
+          <a href="https://www.freecodecamp.org/certification/fanzeng/responsive-web-design">
+            <img src={certificateImages['freecodecamp_responsive_web_design.png']} height="200" width="260"
+              alt="Responsive Web Design"/>
+          </a>
+          <a href="https://www.freecodecamp.org/certification/fanzeng/responsive-web-design" className="certificate-name">
+            Responsive Web Design
+          </a>
+        </div>
+        <div className="certificate-item">
+          <a href="https://www.freecodecamp.org/certification/fanzeng/javascript-algorithms-and-data-structures">
+            <img src={certificateImages['freecodecamp_javascript_algorithms_and_data_structures.png']}
+              height="200" width="260" alt="Javascript Algorithms and Data Structures"/>
+          </a>
+          <a href="https://www.freecodecamp.org/certification/fanzeng/javascript-algorithms-and-data-structures" className="certificate-name">
+            Javascript Algorithms and Data Structures
+          </a>
+        </div>
+        <div className="certificate-item">
+          <a href="https://www.freecodecamp.org/certification/fanzeng/front-end-development-libraries">
+            <img src={certificateImages['freecodecamp_front_end_development_libraries.png']}
+              height="200" width="260" alt="Front End Development Libraries"/>
+          </a>
+          <a href="https://www.freecodecamp.org/certification/fanzeng/front-end-development-libraries" className="certificate-name">
+            Front End Development Libraries
+          </a>
+        </div>
       </div>
-      <div>
-        <table style={{textAlign: 'center'}}>
-          <tr>
-            <td><img src={certificateImages['hackerrank_problem_solving_gold_level_5_star.png']}
-                alt="HackerRank Problem Solving Gold level"/></td>
-          </tr>
-          <tr>
-            <td><a href="https://www.hackerrank.com/fanzeng" className="center_h">HackerRank Problem Solving
-                Gold level</a></td>
-          </tr>
-        </table>
-      </div>
-      <div>
-        <table style={{textAlign: 'center'}}>
-          <tr>
-            <td><img src={certificateImages['hackerrank_sql_gold_level.png']}
-                alt="HackerRank SQL Gold Level"/></td>
-          </tr>
-          <tr>
-            <td><a href="https://www.hackerrank.com/fanzeng" className="center_h">HackerRank SQL Gold level</a>
-            </td>
-          </tr>
-        </table>
-      </div>
-      <div>
-        <table style={{textAlign: 'center'}}>
-          <tr>
-            <td><img src={certificateImages['hackerrank_ruby_gold_level.png']}
-                alt="HackerRank Ruby Gold Level"/></td>
-          </tr>
-          <tr>
-            <td><a href="https://www.hackerrank.com/fanzeng" className="center_h">HackerRank Ruby Gold level</a>
-            </td>
-          </tr>
-        </table>
+      <div className="center_h">
+        <div>
+          <table style={{textAlign: 'center'}}>
+            <tr>
+              <td><img src={certificateImages['hackerrank_java_gold_level.png']}
+                  alt="HackerRank Java Gold level"/></td>
+            </tr>
+            <tr>
+              <td><a href="https://www.hackerrank.com/fanzeng" className="center_h">HackerRank Java Gold level</a>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div>
+          <table style={{textAlign: 'center'}}>
+            <tr>
+              <td><img src={certificateImages['hackerrank_problem_solving_gold_level_5_star.png']}
+                  alt="HackerRank Problem Solving Gold level"/></td>
+            </tr>
+            <tr>
+              <td><a href="https://www.hackerrank.com/fanzeng" className="center_h">HackerRank Problem Solving Gold level</a></td>
+            </tr>
+          </table>
+        </div>
+        <div>
+          <table style={{textAlign: 'center'}}>
+            <tr>
+              <td><img src={certificateImages['hackerrank_sql_gold_level.png']}
+                  alt="HackerRank SQL Gold Level"/></td>
+            </tr>
+            <tr>
+              <td><a href="https://www.hackerrank.com/fanzeng" className="center_h">HackerRank SQL Gold level</a>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div>
+          <table style={{textAlign: 'center'}}>
+            <tr>
+              <td><img src={certificateImages['hackerrank_ruby_gold_level.png']}
+                  alt="HackerRank Ruby Gold Level"/></td>
+            </tr>
+            <tr>
+              <td><a href="https://www.hackerrank.com/fanzeng" className="center_h">HackerRank Ruby Gold level</a>
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   </div>
-</div>
   </>
 }
