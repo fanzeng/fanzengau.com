8ffb6d03e151dd374d58da258314375d17ecb180
Fan Zeng <fanzengau@gmail.com>
2025-05-07 14:02:24 +0000

    Add MathJaxRawTextLoader.jsx

--- client/fanzengau-app/src/myblog/content/statistics/compute_normal_distribution_from_scratch_by_integration/compute_normal_distribution_from_scratch_by_integration.jsx
+++ client/fanzengau-app/src/myblog/content/statistics/compute_normal_distribution_from_scratch_by_integration/compute_normal_distribution_from_scratch_by_integration.jsx
@@ -1,42 +1,14 @@
-import { useState, useEffect } from "react";
-import { MathJax } from "../../../math-jax/MathJax";
-import raw1 from './compute_normal_distribution_from_scratch_by_integration_1.txt';
+import { MathJaxRawTextLoader } from "../../../components/MathJaxRawTextLoader";
 import CodeSnippet from "./CodeSnippet";
+import raw1 from './compute_normal_distribution_from_scratch_by_integration_1.txt';
 import raw2 from './compute_normal_distribution_from_scratch_by_integration_2.txt';
 
 export default function ComputeNormalDistributionFromScratchByIntegration() {
-  let [data1, setData1] = useState('Loading');
-  let [data2, setData2] = useState('Loading');
-
-  useEffect(() => {
-    setTimeout(() => {
-      const nodes = document.getElementsByClassName('data');
-      if (Array.isArray(nodes)) {
-        nodes.forEach(node => {
-          window.MathJax.typesetClear([node]);
-          window.MathJax.typesetPromise([node]);
-        });
-      } else {
-        window.MathJax.typesetClear([nodes]);
-        window.MathJax.typesetPromise([nodes]);
-      }
-    }, 30);
-  }, [data1, data2]);
-
-  fetch(raw1)
-    .then(r => r.text())
-    .then(text => {
-      setData1(text);
-    });
-  fetch(raw2)
-    .then(r => r.text())
-    .then(text => {
-      setData2(text);
-    });
-  return <>
-    <div className="data" dangerouslySetInnerHTML={{ __html: data1 }} />
-    <CodeSnippet />
-    <div className="data" dangerouslySetInnerHTML={{ __html: data2 }} />
-    <MathJax />
-  </>
+  return (
+    <>
+      <MathJaxRawTextLoader file={raw1} className="data" />
+      <CodeSnippet />
+      <MathJaxRawTextLoader file={raw2} className="data" />
+    </>
+  );
 }
