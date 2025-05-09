6f580acfbdc189d9a6a39ce07c7accaa65e67e62
Fan Zeng <fanzengau@gmail.com>
2025-05-09 14:11:20 +0000

    Update menu header for mobile

--- client/fanzengau-app/src/myblog/content/machine_learning/gradient_of_categorical_cross_entropy/gradient_of_categorical_cross_entropy.jsx
+++ client/fanzengau-app/src/myblog/content/machine_learning/gradient_of_categorical_cross_entropy/gradient_of_categorical_cross_entropy.jsx
@@ -1,4 +1,3 @@
-import { MathJax } from "../../../math-jax/MathJax";
 import { MathJaxRawTextLoader } from "../../../components/MathJaxRawTextLoader";
 import raw from './gradient_of_categorical_cross_entropy.txt';
 
